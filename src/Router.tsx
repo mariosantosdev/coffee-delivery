b93bbb8a1cbac7be670a29be4474481f1f69e7aa
Mário Santos <mariodev7@gmail.com>
2023-02-21 13:23:54 -0300

    using public layout on routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { HomePage } from "~/pages/Home";
+import { PublicLayout } from "~/layouts/Public";
 
 export const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<PublicLayout />}>
+        <Route path="/" element={<HomePage />} />
+      </Route>
     </Routes>
   );
 };
