fd90702accf9cdeec8d8ae251d53f77c03cefbb1
Mário Santos <mariodev7@gmail.com>
2023-02-21 15:00:01 -0300

    formatting coffes when get in api

--- api/routes.ts
+++ api/routes.ts
@@ -1,8 +1,17 @@
 import { Router } from "express";
-import { coffes } from "./db.json";
+import { coffes as rawCoffes } from "./db.json";
 
 const router = Router();
 
-router.get("/coffes", (_req, res) => res.status(200).json(coffes));
+router.get("/coffes", (req, res) => {
+  const baseURL = req.protocol + "://" + req.get("host");
+
+  const coffes = rawCoffes.map((coffe) => ({
+    ...coffe,
+    image: `${baseURL}/api${coffe.image}`,
+  }));
+
+  res.status(200).json({ coffes });
+});
 
 export default router;
