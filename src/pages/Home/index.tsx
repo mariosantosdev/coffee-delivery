2c7528a5bb374558685a98f04cbbac6d9dfba017
Mário Santos <mariodev7@gmail.com>
2023-02-21 15:40:08 -0300

    create home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,28 @@
+import { Fragment, useEffect, useState } from "react";
+import { CoffeCard, Coffe } from "./components/CoffeCard";
 import { HomeHero } from "./components/Hero";
+import { CoffeContainer, GridCoffees } from "./styles";
 
 export function HomePage() {
-  return <HomeHero />;
+  const [coffes, setCoffes] = useState<Coffe[]>([]);
+
+  useEffect(() => {
+    fetch("/api/coffes")
+      .then((response) => response.json())
+      .then((data) => setCoffes(data.coffes));
+  }, []);
+
+  return (
+    <Fragment>
+      <HomeHero />
+      <CoffeContainer>
+        <h2>Nossos cafés</h2>
+        <GridCoffees>
+          {coffes.map((coffe) => (
+            <CoffeCard key={coffe.slug} coffe={coffe} />
+          ))}
+        </GridCoffees>
+      </CoffeContainer>
+    </Fragment>
+  );
 }
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const CoffeContainer = styled.div`
+  margin-top: 2rem;
+  display: flex;
+  flex-direction: column;
+
+  h2 {
+    color: ${({ theme }) => theme.colors.base_subtitle};
+    font-size: 2rem;
+    font-family: "Baloo 2", cursive;
+    font-weight: 800;
+  }
+`;
+
+export const GridCoffees = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 2rem;
+  margin-top: 2rem;
+  justify-items: center;
+`;
