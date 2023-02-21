17339ddd041e933bacd098bef874ee33fab7a2b5
Mário Santos <mariodev7@gmail.com>
2023-02-21 13:23:42 -0300

    create public layout

---
+++ src/layouts/Public/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+  height: 100vh;
+  max-width: 1120px;
+
+  margin: 0 auto;
+  padding: 0 0.5rem;
+`;
