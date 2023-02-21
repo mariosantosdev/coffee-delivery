99d3a403ce1581e1a60b94927bf4f3720045bce9
Mário Santos <mariodev7@gmail.com>
2023-02-21 12:56:23 -0300

    setup react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "~/styles/global";
 import { theme } from "~/styles/theme";
+import { Router } from "./Router";
 
 export function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <h1>Hello World</h1>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <Router />
 
-      <GlobalStyle />
-    </ThemeProvider>
+        <GlobalStyle />
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
